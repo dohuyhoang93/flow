bc49e86b5285f9f9917b9a9d3c2d1e1dd13ef3f5
DoHuyHoang <dohuyhoangvn93@gmail.com>
2024-10-23 16:39:23 +0700

    day là commit lần 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Hello, Đây là chương trình dùng để test flow");
+    println!("Hello, Đây là chương trình dùng để test flow với loop");
 
     let mut count = 0;
     'counting_up: loop {
@@ -20,4 +20,36 @@ fn main() {
         count +=1;
     }
     println!("End count = {}", count);
+
+    println!("Đây là mã test flow - while");
+    let mut number = 3;
+
+    while number != 0 {
+        println!("số đã cho khác 0 là: {}!", number);
+
+        number -= 1;
+    }
+    println!("LiftOff!!!");
+
+    println!("Đây là code test flow - while với mảng");
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 { //cách tiếp cận dễ gây lỗi truy cập quá phạm vi
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    println!("Đây là code test flow - for");
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..=10).rev() {
+        println!("the value is: {}", number);
+    }
+    println!("LiftOff");
 }
